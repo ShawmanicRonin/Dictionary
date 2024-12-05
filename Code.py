31f006fead059358a9297142f2789adde40b5669
Ronin Evans <41272060+ShawmanicRonin@users.noreply.github.com>
2024-12-05 02:10:34 -0800

    Began reworking serch func within the class this time and calling it as such.

--- Code.py
+++ Code.py
@@ -2,39 +2,31 @@ import sys
 import webbrowser
 import string
 from Dictionary import DictionaryClass
-choice_separator = '-' * 5
+ChoiceSeparator = '-' * 5
 
 def Menu():
     print(f'\nWhat would you like to do?\n\n',
-          'Search the Dictionary', choice_separator, '\b----- 1\n',
-          'Retrieve a random entry', choice_separator, '\b--- 2\n',
-          'Append the Dictionary', choice_separator, '\b----- 3\n',
-          'Exit the Dictionary', choice_separator, '\b------- 4')
-    menu_choice = int(input('>'))
-    if menu_choice == 1:
-        search()
-    elif menu_choice == 2:
-        random_word()
-    elif menu_choice == 3:
-        append()
-    elif menu_choice == 4:
-        exit()
+          'Search the Dictionary', ChoiceSeparator, '\b----- 1\n',
+          'Retrieve a random entry', ChoiceSeparator, '\b--- 2\n',
+          'Append the Dictionary', ChoiceSeparator, '\b----- 3\n',
+          'Exit the Dictionary', ChoiceSeparator, '\b------- 4\n\n\n')
+    MenuChoice = int(input('>>'))
+    if MenuChoice == 1:
+        DictionaryClass.Search()
+    elif MenuChoice == 2:
+        RandomWord()
+    elif MenuChoice == 3:
+        Append()
+    elif MenuChoice == 4:
+        Exit()
 
-def search():
-    search_input = (str(input(f'\nWhat word would you like to define?\n\n\n>')))
-    try:
-        if search_input in DictionaryClass.DictionaryVar.keys:
-            print('I work')
-    except:
-        print('I dont work')
-
-def random_word():
+def RandomWord():
     pass
 
-def append():
+def Append():
     pass
 
-def exit():
+def Exit():
     webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
     sys.exit
 
--- Dictionary.py
+++ Dictionary.py
@@ -4,6 +4,16 @@ class DictionaryClass:
     def __init__(self, word, definition):
         self.word = word
         self.definition = definition
+        DictionaryEntry = word + definition
+
+    def Search():
+        Word = str.lower(input("What word would you like to define?\n\n\n>>"))
+        try:
+            if Word in DictionaryClass.DictionaryVar.keys:
+                return DictionaryClass.DictionaryVar[Word]
+        except:
+            print('I don\'t work')
+
 
     @classmethod
     def congragate_data(self, word, definition):
