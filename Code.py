580ed1e2cf43c7228022236c424ca3115a3cfade
Ronin Evans <41272060+ShawmanicRonin@users.noreply.github.com>
2024-12-06 14:05:56 -0700

    possibly removing class

--- Code.py
+++ Code.py
@@ -1,9 +1,11 @@
 import sys
 import webbrowser
 import string
-from Dictionary import DictionaryClass
+import random
 ChoiceSeparator = '-' * 5
 ErrorMessage = 'I don\'t work.'
+
+DictionaryVar = {"house" : "this is not for you", "father" : "no, \"I\" am your father!"}
 
 def Menu():
     print(f'\nWhat would you like to do?\n\n',
@@ -13,13 +15,39 @@ def Menu():
           'Exit the Dictionary', ChoiceSeparator, '\b------- 4\n\n\n')
     MenuChoice = int(input('Please make a selection\n>>'))
     if MenuChoice == 1:
-        DictionaryClass.Search()
+        Search()
     elif MenuChoice == 2:
-        DictionaryClass.RandomWord()
+        RandomWord()
     elif MenuChoice == 3:
-        DictionaryClass.AppendDict()
+        AppendDict()
     elif MenuChoice == 4:
         Exit()
+
+def Search():
+    try:
+        Word = str.lower(input("What word would you like to define?\n\n\n>>"))
+        DictionaryEntry = DictionaryVar.get(Word)
+        if Word in DictionaryVar.keys:
+            return DictionaryEntry.capitalize
+    except:
+        ErrorMessage
+
+def RandomWord():
+    try:
+        RandomResult = random.choice(list(DictionaryVar.items))
+        return RandomResult
+    except:
+        ErrorMessage
+
+def AppendDict():
+    try:
+        NewWord = str(input('What word would you like to define?\n\n\n>>'))
+        NewDef = str(input(f'What is the definition of {NewWord}?\n\n\n>>'))
+        DictionaryVar.update({NewWord:NewDef})
+        print(DictionaryVar)
+    except:
+        ErrorMessage
+
 
 def Exit():
     webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
